c5712504c484c038b84194eb9ce768c5d0ca163d
Abhinav Gupta <45824234+rgabhi@users.noreply.github.com>
2019-06-05 00:59:50 +0530

    userRoute added

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
 
+const userModel =require('../models/userModel');
+
+router.get('/',function(req,res){
+    res.send("User's Home").status(200);
+});
+
+router.post('/',function(req,res){
+    const newUser = new userModel({
+        _id : new mongoose.Types.ObjectId(),
+        name : req.body.name,
+        email : req.body.email,
+        password : req.body.password
+    });
+
+    userModel.find({email : req.body.email})
+    .exec()
+    .then(users=>{
+        if(users.length>0){
+            res.send("Account already exists").status(400);
+        }
+        else{
+            newUser.save();
+            res.send("Account created").status(201);
+        }
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+});
+
+
+
+module.exports = router;
